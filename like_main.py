3e5425ecee80f94821d581e5fff3faeeb5c1e296
readoappnewtest <72143403+readoappnewtest@users.noreply.github.com>
2021-05-19 17:16:00 +0530

    Update like_main.py

--- like_main.py
+++ like_main.py
@@ -63,7 +63,7 @@ for api in json_js:
         #body= '{"message":"Hiaryanj","receiver_id":"u8CU5TC9eBeongBvULTmFSlw4cY2"}'
         #body = '{"message":"Hi, hwo are you","receiver_id":"r8Xmgyr9p8RJDuulGTT5Aaxuln02"}'
         #body = '{"like_ids":["sp_cei4asxjw3ybl"],"unlike_ids":[]}'
-        body = '{"like_ids":["sp_qqtcwhxobykvj"],"unlike_ids":[]}'
+        body = '{"like_ids":["sp_av6sn5otuwzio"],"unlike_ids":[]}'
 
         data = requests.post(url,headers=header,data=body)
         print(data)
